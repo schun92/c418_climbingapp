8c2c632090bd25d8b9d06cf20ba364b952d9eef2
schun92 <sarahsandiferchun@gmail.com>
2018-06-25 16:00:10 -0700

    created filter button and modal in react. need to style it more

---
+++ frontend/src/components/results-filter-btn.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import FilterModal from './results-filter-modal';
+
+class FilterBtn extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            showFilterModal: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleHideModal = this.handleHideModal.bind(this);
+    }
+
+    handleClick(e) {
+        this.setState({
+            showFilterModal: true
+        });
+    }
+
+    handleHideModal() {
+        this.setState({
+            showFilterModal: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="filter">
+                    <button className="filter-modal-button" type="button" onClick={this.handleClick}>Filter</button>
+                </div>
+                {this.state.showFilterModal ? <FilterModal handleHideModal={this.handleHideModal} /> : null}
+            </div>
+        )
+
+    }
+
+}
+
+
+
+export default FilterBtn;
