74c5e6b5aa58cb1ef24621d0107d2fd85f9eda98
Paul Lujan <paulglujan@gmail.com>
2018-06-26 11:32:18 -0700

    Added route detail dummy data

---
+++ dummy_data/route_details_dummy_data.js
@@ -0,0 +1,15 @@
+//Request from front end
+var data_call = {
+    routeID: 29,
+}
+
+//Response from backend 
+var routeDetailsDummyData = {
+    mountainLocation: 'Ortega Falls',
+    routeName: 'Dihedral',
+    difficulty: 5.7,
+    popularity: 3.1,
+    popVotes: 49,
+    climbType: 'Trad, TR',
+    pic: 'https://cdn-files.apstatic.com/climb/112528611_medium_1494310683.jpg',
+}
