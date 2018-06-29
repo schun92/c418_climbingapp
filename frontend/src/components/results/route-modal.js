1bf9bbeaa86ce223fed2835d32a34ab92ade8552
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-06-29 15:24:45 -0700

    added funtional scrolling bottom bar

--- frontend/src/components/results/route-modal.js
+++ frontend/src/components/results/route-modal.js
@@ -2,68 +2,70 @@ import React, { Component } from 'react';
 import './route-modal.css';
 
 class RouteModal extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         console.log(this.props);
-        return(
+        return (
             <div className={`routes-modal ${this.props.display}`} >
-            <h1>Pirate's Cove</h1>
-            <p>7 routes</p>
-            <ul>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Not Even</p>
-                        <p>3.8</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Iron Man</p>
-                        <p>v3</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Diamond Man</p>
-                        <p>v4</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Paper Boy</p>
-                        <p>V7</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Tin Man</p>
-                        <p>V0+</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Italian Fall</p>
-                        <p>V3</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-                <li>
-                    <a href="./route-details-page.html">
-                        <p>Not Even Chips</p>
-                        <p>V3- PG13</p>
-                        <i className="material-icons">arrow_forward_ios</i>
-                    </a>
-                </li>
-            </ul>
-        </div>
+                <div onClick = {this.props.handleClick}>
+                    <h1>Pirate's Cove</h1>
+                    <p>7 routes</p>
+                </div>
+                <ul>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Not Even</p>
+                            <p>3.8</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Iron Man</p>
+                            <p>v3</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Diamond Man</p>
+                            <p>v4</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Paper Boy</p>
+                            <p>V7</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Tin Man</p>
+                            <p>V0+</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Italian Fall</p>
+                            <p>V3</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="./route-details-page.html">
+                            <p>Not Even Chips</p>
+                            <p>V3- PG13</p>
+                            <i className="material-icons">arrow_forward_ios</i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         )
     }
 }
