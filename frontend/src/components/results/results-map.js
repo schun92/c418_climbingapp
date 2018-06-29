fa75ef350d6d4fabf225fe97f30feefc1f8d2016
schun92 <sarahsandiferchun@gmail.com>
2018-06-29 10:21:23 -0700

    working on the onClick function on the map image

--- frontend/src/components/results/results-map.js
+++ frontend/src/components/results/results-map.js
@@ -4,20 +4,17 @@ import map from '../../assets/images/map.png'
 import './results-map.css';
 
 
+
 class RouteMap extends Component {
     constructor(props){
         super(props)
     
     }
 
-TogglePullUpBar(){
-
-}
-
     render(){
         return(
             <div>
-                <img className="map-img" src={map} onClick = {}/>
+                <img onClick = {this.props.toggle} className="map-img" src={map} />
             </div>
         )
     }
