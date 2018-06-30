8f712edb9aaf10b0b125d52a830f6a57bdb81005
jhon alino <jhonfrancisalino@gmail.com>
2018-06-30 12:25:17 -0700

    added map click and change the displayed mountain location

--- frontend/src/components/results/results-map.js
+++ frontend/src/components/results/results-map.js
@@ -5,43 +5,39 @@ import "./results-map.css";
 import mapStyle from "./map-style.json";
 
 class RouteMap extends Component {
-  constructor(props) {
-    super(props);
-    // this.key = "AIzaSyBve0XPbDvrdzCZ18vda59aGZxBp8v_lU4";
-    this.maps = null;
-    this.ref = React.createRef();
-  }
+	constructor(props) {
+		super(props);
+		// this.key = "AIzaSyBve0XPbDvrdzCZ18vda59aGZxBp8v_lU4";
+		this.maps = null;
+		this.ref = React.createRef();
+	}
 
-  componentDidMount() {
-    console.log(mapStyle);
-    this.map = new google.maps.Map(this.ref.current, {
-      center: { lat: 33.6251, lng: -117.4273 },
-      zoom: 12,
-      styles: mapStyle
-    });
-  }
+	componentDidMount() {
+		this.map = new google.maps.Map(this.ref.current, {
+			center: { lat: 33.6251, lng: -117.4273 },
+			zoom: 12,
+			styles: mapStyle
+		});
+	}
 
-  render() {
-    this.props.locations.map(({ latitude: lat, longitude: lng }) => {
-      var marker = new google.maps.Marker({
-        position: { lat, lng },
-        map: this.map
-      });
+	render() {
+		this.props.locations.map(location => {
+			const { latitude: lat, longitude: lng } = location;
+			var marker = new google.maps.Marker({
+				position: { lat, lng },
+				map: this.map
+			});
 
-      marker.addListener("click", function(e) {
-        console.log(e);
-        this.map.setZoom(15);
-        this.map.setCenter(marker.getPosition());
-      });
-    });
+			marker.addListener("click", () => this.props.selectLocation(location));
+		});
 
-    return (
-      //   <div className="map-container" onClick={this.props.handleClick}>
-      <div className="map-container">
-        <div ref={this.ref} />
-      </div>
-    );
-  }
+		return (
+			//   <div className="map-container" onClick={this.props.handleClick}>
+			<div className="map-container">
+				<div ref={this.ref} />
+			</div>
+		);
+	}
 }
 
 export default RouteMap;
