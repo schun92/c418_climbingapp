b38fc0d2369d61a51306d4df4c06f8bd67bcedfc
jhonalino <jhonfrancisalino@gmail.com>
2018-06-29 21:39:42 -0700

    added google maps to results page

--- frontend/src/components/results/results-map.js
+++ frontend/src/components/results/results-map.js
@@ -1,41 +1,47 @@
-import React, { Component }from 'react';
-import RouteModal from './route-modal';
-import map from '../../assets/images/map.png'
-import './results-map.css';
-
-
+import React, { Component } from "react";
+import RouteModal from "./route-modal";
+import map from "../../assets/images/map.png";
+import "./results-map.css";
+import mapStyle from "./map-style.json";
 
 class RouteMap extends Component {
-    constructor(props){
-        super(props)
-        console.log('props:', props)
-    
-    }
+  constructor(props) {
+    super(props);
+    // this.key = "AIzaSyBve0XPbDvrdzCZ18vda59aGZxBp8v_lU4";
+    this.maps = null;
+    this.ref = React.createRef();
+  }
 
-    render(){
-        return(
-            <div className="map-container" onClick={this.props.handleClick}>
-                <img className="map-img" src={map} />
-            </div>
-        )
-    }
+  componentDidMount() {
+    console.log(mapStyle);
+    this.map = new google.maps.Map(this.ref.current, {
+      center: { lat: 33.6251, lng: -117.4273 },
+      zoom: 12,
+      styles: mapStyle
+    });
+  }
+
+  render() {
+    this.props.locations.map(({ latitude: lat, longitude: lng }) => {
+      var marker = new google.maps.Marker({
+        position: { lat, lng },
+        map: this.map
+      });
+
+      marker.addListener("click", function(e) {
+        console.log(e);
+        this.map.setZoom(15);
+        this.map.setCenter(marker.getPosition());
+      });
+    });
+
+    return (
+      //   <div className="map-container" onClick={this.props.handleClick}>
+      <div className="map-container">
+        <div ref={this.ref} />
+      </div>
+    );
+  }
 }
 
 export default RouteMap;
-
-
-    // document.querySelector('.map-content').addEventListener('click', function () {
-    //     document.querySelector('.routes-modal').classList.toggle('show');
-    // })
-    // document.querySelector('#modalButton').addEventListener('click', showModal)
-    // document.querySelector('#modalShadow').addEventListener('click', noneModal)
-    // document.querySelector('#modalBody').addEventListener('click', e => e.stopPropagation())
-
-    // function showModal(e) {
-
-    //     document.querySelector('#modalShadow').style.display = "block"
-    // }
-
-    // function noneModal() {
-    //     document.querySelector('#modalShadow').style.display = "none"
-    // }
