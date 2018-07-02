7b574717aabf46c70665ae96eabc3e98b362bc96
Paul Lujan <paulglujan@gmail.com>
2018-07-02 13:06:09 -0700

    Added route details endpoint

---
+++ climbingappPHP/frontendAPI/get_route_details.php
@@ -0,0 +1,30 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "root", "mountainproject");
+
+$inputID = $_GET['data'];
+
+$output = [
+	'success'=> false,
+	'errors'=>[]
+];
+
+$query = "SELECT `name`, `location_mountain` AS 'location', `description`, `type`, `pitches`, `difficulty`, `stars`, `star_votes`, `image` FROM `routes` WHERE `id` = $inputID";
+
+$result = mysqli_query($conn, $query);
+
+if (empty($result)) {
+	$output['errors'][] = 'database error';
+} else {
+	if ($result) {
+		$output['success'] = true;
+        $output['data']=[];
+        while( $row = mysqli_fetch_assoc($result)){
+            $output['data'][] = $row;
+        };
+	} else {
+		$output['errors'][] = 'no data';
+	};
+};
+
+$outputJSON = json_encode($output);
+print_r($outputJSON);
