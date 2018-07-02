148087cb9aff146a05c538a160e476436b8491bf
Amin Ghanimati <AminG3M@yahoo.com>
2018-07-02 16:01:08 -0700

    Created queries to select table rows that match with map info

--- climbingappPHP/frontendAPI/get_location_data.php
+++ climbingappPHP/frontendAPI/get_location_data.php
@@ -6,23 +6,25 @@ $output = [
 	'errors'=>[]
 ];
 
-$latQuery = "SELECT `ID`, `name`, `avgLat`, `avgLong`, `numRoutes` FROM locations WHERE (avgLat BETWEEN 33.6163 AND 33.8512) AND (avgLong BETWEEN -117.9008 AND -117.5991)";
+$query = "SELECT `ID`, `name`, `avgLat`, `avgLong`, `numRoutes` FROM locations WHERE (avgLat BETWEEN 33.6163 AND 33.8512) AND (avgLong BETWEEN -117.9008 AND -117.5991)";
 
-$result = mysqli_query($conn, $latQuery);
+$result = mysqli_query($conn, $query);
 
 if (empty($result)) {
-	$output['errors'][] = 'database error';
+	$output['errors'][] = 'Database Error';
 } else {
-	if ($result) {
+	if (mysqli_num_rows($result) > 0) {
 		$output['success'] = true;
         $output['data']=[];
         while( $row = mysqli_fetch_assoc($result)){
             $output['data'][] = $row;
         };
 	} else {
-		$output['errors'][] = 'no data';
+		$output['errors'][] = 'No Data';
 	};
 };
 
 $outputJSON = json_encode($output);
 print_r($outputJSON);
+
+?>
