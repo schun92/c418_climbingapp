2fd7d39903f35458b09152d5c09504e51a4561f6
Paul Lujan <paulglujan@gmail.com>
2018-07-02 16:16:30 -0700

    Linked Google Places cURL call to get_location_data

--- climbingappPHP/frontendAPI/get_location_data.php
+++ climbingappPHP/frontendAPI/get_location_data.php
@@ -1,14 +1,17 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "root", "mountainproject");
 
+require('../backendAPI/google_places_curl.php');
+
 $output = [
 	'success'=> false,
 	'errors'=>[]
 ];
 
-$latQuery = "SELECT `ID`, `name`, `avgLat`, `avgLong`, `numRoutes` FROM locations WHERE (avgLat BETWEEN 33.6163 AND 33.8512) AND (avgLong BETWEEN -117.9008 AND -117.5991)";
+// $query = "SELECT `ID`, `name`, `avgLat`, `avgLong`, `numRoutes` FROM locations WHERE (avgLat BETWEEN 33.6163 AND 33.8512) AND (avgLong BETWEEN -117.9008 AND -117.5991)";
+$query = "SELECT `ID`, `name`, `avgLat`, `avgLong`, `numRoutes` FROM locations";
 
-$result = mysqli_query($conn, $latQuery);
+$result = mysqli_query($conn, $query);
 
 if (empty($result)) {
 	$output['errors'][] = 'database error';
@@ -17,12 +20,15 @@ if (empty($result)) {
 		$output['success'] = true;
         $output['data']=[];
         while( $row = mysqli_fetch_assoc($result)){
-            $output['data'][] = $row;
+            $output['data']['locations'][] = $row;
         };
 	} else {
 		$output['errors'][] = 'no data';
 	};
 };
+
+$output['data']['mapCenterLat'] = $lat;
+$output['data']['mapCenterLon'] = $lon;
 
 $outputJSON = json_encode($output);
 print_r($outputJSON);
