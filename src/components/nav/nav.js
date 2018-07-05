828138b93cd2d8838791230afec98ab6b096862b
jhonalino <jhonfrancisalino@gmail.com>
2018-07-05 00:49:54 -0700

    added burger menu cross close animation

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,56 +1,57 @@
-import React, { Component } from 'react';
-import './nav.css';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import "./nav.css";
+import { NavLink } from "react-router-dom";
 
 class Nav extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props);
 
-        this.goBack = this.goBack.bind(this);
-        this.toggleBurgerMenu = this.toggleBurgerMenu.bind(this);
-        this.state = {
-            showNavMenu: false
-        }
+		this.goBack = this.goBack.bind(this);
+		this.handleBurgerMenuClick = this.handleBurgerMenuClick.bind(this);
+		this.state = {
+			showNavMenu: false
+		};
+	}
+	goBack() {
+		window.history.back();
+	}
 
-    }
-    goBack() {
-        window.history.back()
-    }
+	handleBurgerMenuClick() {
+		this.setState({ showNavMenu: !this.state.showNavMenu });
+	}
 
-    toggleBurgerMenu() {
-        this.setState({ showNavMenu: !this.state.showNavMenu })
-    }
-
-    render() {
-        return (
-            <nav>
-                <i className='material-icons' onClick={this.goBack}>
-                    arrow_back_ios
-            </i>
-                <div className='brand'>CJ</div>
-                <i className='material-icons burger-menu' onClick={this.toggleBurgerMenu}>
-                    menu
-            </i>
-                <ul className={this.state.showNavMenu ? 'nav-ul show' : 'nav-ul'} onClick={this.toggleBurgerMenu}>
-                    <li>
-                        <NavLink to='/'>home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/itinerary'>itinerary</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/about'>about</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/team'>team</NavLink>
-                    </li>
-                </ul>
-
-            </nav>
-        )
-    }
+	render() {
+		return (
+			<nav className="navbar">
+				<i className="material-icons" onClick={this.goBack}>
+					arrow_back_ios
+				</i>
+				<div className="brand">CJ</div>
+				<div
+					className={this.state.showNavMenu ? "burger-menu close" : "burger-menu"}
+					onClick={this.handleBurgerMenuClick}
+				>
+					<span />
+					<span />
+					<span />
+				</div>
+				<ul className={this.state.showNavMenu ? "show" : ""} onClick={this.toggleBurgerMenu}>
+					<li>
+						<NavLink to="/">home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/itinerary">itinerary</NavLink>
+					</li>
+					<li>
+						<NavLink to="/about">about</NavLink>
+					</li>
+					<li>
+						<NavLink to="/team">team</NavLink>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
 }
 
 export default Nav;
-
-
