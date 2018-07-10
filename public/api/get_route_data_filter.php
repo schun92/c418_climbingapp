011ddaa62fdb1e6af774ff42a4a6a434be1f5ab2
Paul Lujan <paulglujan@gmail.com>
2018-07-10 14:31:26 -0700

    Added endpoint pull up bar when a filter is on

---
+++ public/api/get_route_data_filter.php
@@ -0,0 +1,33 @@
+<?php
+//Request from frontend is
+//routeList=106070976,105903225,105870999
+ header("Access-Control-Allow-Origin: *");
+$routeList = $_GET['routeList'];
+
+$conn = mysqli_connect("localhost", "root", "root", "mountainproject");
+
+
+$output = [
+    "success" => false,
+    "error" => []
+];
+
+
+$query = "SELECT `id`, `name`, `type`, `difficulty` FROM `routes` WHERE `id` IN ({$routeList})";
+$result = mysqli_query($conn, $query);
+
+if(empty($result)) {
+    $output['data'][] = 'Database Error';
+} else {
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            $output['data']['routes'][] = $row;
+        }
+        $output['success'] = true;
+    } else {
+        $output['success'] = 'No Data';
+    }
+}
+
+$outputJSON = json_encode($output);
+print_r($outputJSON);
