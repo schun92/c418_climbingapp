648eeb7b8a2c862fcc8a992399b4b5da3aa3c79c
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-07-14 12:41:56 -0700

    itineraryBugFix

--- src/reducers/itinerary_reducer.js
+++ src/reducers/itinerary_reducer.js
@@ -7,10 +7,23 @@ const DEFAULT_STATE = {
 function itineraryReducer(state = DEFAULT_STATE, action) {
 	switch (action.type) {
 		case types.ADD_ROUTE_TO_ITINERARY:
-			return { ...state, routes: [...state.routes, action.payload] };
+			let isUnique = true;
+
+			state.routes.map( route => {
+				if(route.id === action.payload.id){
+					isUnique = false;
+				}
+			});
+			console.log('ACTION PAYLOAD', action.payload)
+			console.log('STATE.ROUTES', state.routes)
+			if(isUnique){
+				return { ...state, routes: [...state.routes, action.payload] };
+			}
+
+			return state;
 		case types.REMOVE_ROUTE_FROM_ITINERARY:
-			console.log('action, and s.routes', action, state.routes)
-			return { ...state, routes: state.routes.filter(route => route.id !== action.payload.id) } 
+			console.log('action, and s.routes', action, 'state.routes', state.routes)
+			return { ...state, routes: state.routes.filter(route => route.id !== action.payload) } 
 		default:
 			return state;
 	}
