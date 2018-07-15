8ec85763b0bbb40225a4ffccb8cd4b3e7914d5ae
jhonalino <jhonfrancisalino@gmail.com>
2018-07-15 15:56:56 -0700

    when route is shared clicking back now goes to search page

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./nav.css";
 import { NavLink, withRouter } from "react-router-dom";
-import brandLogo from '../../assets/images/pf_cropped.png';
+import brandLogo from "../../assets/images/pf_cropped.png";
 
 class Nav extends Component {
 	constructor(props) {
@@ -10,14 +10,16 @@ class Nav extends Component {
 		this.goBack = this.goBack.bind(this);
 		this.toggleBurgerMenu = this.toggleBurgerMenu.bind(this);
 		this.state = {
-			showNavMenu: false,
-
+			showNavMenu: false
 		};
 	}
 
 	goBack() {
-		window.history.back();
-	
+		if (this.props.history.length < 3) {
+			this.props.history.replace("/");
+		} else {
+			this.props.history.goBack();
+		}
 	}
 
 	toggleBurgerMenu() {
@@ -25,6 +27,7 @@ class Nav extends Component {
 	}
 
 	render() {
+		console.log(this.props.history);
 		return (
 			<nav className="navbar is-primary is-text-white">
 				{this.props.location.pathname === "/" ? null : (
@@ -32,9 +35,11 @@ class Nav extends Component {
 						arrow_back_ios
 					</i>
 				)}
-				
-					<NavLink to="/"><img className='brand' src={brandLogo}/></NavLink>
-				
+
+				<NavLink to="/">
+					<img className="brand" src={brandLogo} />
+				</NavLink>
+
 				<div
 					className={this.state.showNavMenu ? "burger-menu close" : "burger-menu"}
 					onClick={this.toggleBurgerMenu}
